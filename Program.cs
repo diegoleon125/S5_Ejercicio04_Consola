10bfbeb58d241561ec4749936e25648ffd8c47c8
user <user@LAPTOP-2DK3J1EI>
2023-09-20 22:20:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("Ingrese el número límite: ");
+int limite = int.Parse(Console.ReadLine());
+Console.WriteLine("============================");
+Console.WriteLine("Escoja una opción:");
+Console.WriteLine("1: Números pares");
+Console.WriteLine("2: Números impares");
+Console.WriteLine("3: Factorial");
+Console.Write("-> ");
+int opcion = int.Parse(Console.ReadLine());
+switch (opcion)
+{
+    case 1:
+        for (int i = 1; i <= limite; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.Write(i + ",");
+            }
+        }
+        break;
+    case 2:
+        for (int i = 1; i <= limite; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.Write(i + ",");
+            }
+        }
+        break;
+    case 3:
+        long resultado = 1;
+        for (int i = 1; i <= limite; i++)
+        {
+            resultado *= i;
+        }
+        Console.Write("El factorial es " + resultado);
+        break;
+}
